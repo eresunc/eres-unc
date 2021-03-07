affd77e052a26b00b2bb0dcaa26d7895b62f4d3a
eresunc <eres.unc.cajamarca@gmail.com>
2021-03-07 16:59:28 -0500

    Add other test

--- js/fetch_posts.js
+++ js/fetch_posts.js
@@ -1,8 +1,16 @@
 console.log("Start call to FB")
-FB.api("/eres.unc.cajamarca/posts/", function (res) {
+FB.api("/eres.unc.cajamarca/posts", function (res) {
   console.log("RES", res)
   //   for (var i = 0; i < response.data.length; i++) {
   //     alert(response.data[i].message)
   //   }
 })
 console.log("END call")
+
+FB.api("/eres.unc.cajamarca/feed", function (response) {
+  console.log("resp 2")
+  if (response && !response.error) {
+    /* handle the result */
+    console.log("res2", response)
+  }
+})
