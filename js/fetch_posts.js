d9ae8015c41b88d8234b4ba96edde83827900ef7
eresunc <eres.unc.cajamarca@gmail.com>
2021-03-07 17:05:40 -0500

    Test

--- js/fetch_posts.js
+++ js/fetch_posts.js
@@ -1,16 +1,20 @@
-console.log("Start call to FB")
-FB.api("/eres.unc.cajamarca/posts", function (res) {
-  console.log("RES", res)
-  //   for (var i = 0; i < response.data.length; i++) {
-  //     alert(response.data[i].message)
-  //   }
-})
-console.log("END call")
+// console.log("Start call to FB")
+// FB.api("/eres.unc.cajamarca/posts", function (res) {
+//   console.log("RES", res)
+//   //   for (var i = 0; i < response.data.length; i++) {
+//   //     alert(response.data[i].message)
+//   //   }
+// })
+// console.log("END call")
 
-FB.api("/eres.unc.cajamarca/feed", function (response) {
-  console.log("resp 2")
-  if (response && !response.error) {
-    /* handle the result */
-    console.log("res2", response)
-  }
-})
+// FB.api("/eres.unc.cajamarca/feed", function (response) {
+//   console.log("resp 2")
+//   if (response && !response.error) {
+//     /* handle the result */
+//     console.log("res2", response)
+//   }
+// })
+
+fetch("https://www.facebook.com/pg/eres.unc.cajamarca/posts/").then((res) =>
+  console.log(res)
+)
