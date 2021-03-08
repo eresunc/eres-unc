a85f78ca0a59614610e771c6d063e4bd44cb17c7
eresunc <eres.unc.cajamarca@gmail.com>
2021-03-08 15:45:30 -0500

    Solved hover bug

--- js/mostrar_curriculum.js
+++ js/mostrar_curriculum.js
@@ -5,15 +5,38 @@ let modalFooter = document.querySelector("#curriculumModal .modal-footer")
 let verPerfilBtns = document.querySelectorAll(".ver-perfil")
 let cardMembers = document.querySelectorAll(".team-member-card")
 
-for (let perfil of verPerfilBtns) {
-  perfil.addEventListener("click", (e) => {
-    let member = e.target
-    console.log(member.id)
-    let curr = curriculums[member.id]
-    let cardMember = member.parentNode.parentNode
+for (let btnPerfil of verPerfilBtns) {
+  btnPerfil.addEventListener("click", (e) => {
+    let _btnPerfil = e.target
+    console.log(_btnPerfil.id)
+    let curr = curriculums[_btnPerfil.id]
 
     modalTitle.innerHTML = curr.nombre
     modalDesc.innerHTML = curr.html
     modalFooter.innerHTML = curr.redes
+
+    let cardMember = _btnPerfil.parentNode.parentNode
+    let content = cardMember.innerHTML
+    cardMember.innerHTML = content
+
+    refreshElement(_btnPerfil.id)
+  })
+}
+
+function refreshElement(btnPerfilId) {
+  let btnPerfil = document.getElementById(btnPerfilId)
+  btnPerfil.addEventListener("click", (e) => {
+    let _btnPerfil = e.target
+    let curr = curriculums[_btnPerfil.id]
+
+    modalTitle.innerHTML = curr.nombre
+    modalDesc.innerHTML = curr.html
+    modalFooter.innerHTML = curr.redes
+
+    let cardMember = _btnPerfil.parentNode.parentNode
+    let content = cardMember.innerHTML
+    cardMember.innerHTML = content
+
+    refreshElement(_btnPerfil.id)
   })
 }
