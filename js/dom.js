fbb77e8e368a359c99b404ae8fcc3bba4e0742cb
eresunc <eres.unc.cajamarca@gmail.com>
2021-03-09 13:49:29 -0500

    Add css style to fix redirection on hidden icons

--- js/dom.js
+++ js/dom.js
@@ -9,9 +9,9 @@ var sticky = navbar.offsetTop
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
-    redesCirculo.classList.add("opacity-1")
+    redesCirculo.classList.add("d-opacity-1")
   } else {
     navbar.classList.remove("sticky")
-    redesCirculo.classList.remove("opacity-1")
+    redesCirculo.classList.remove("d-opacity-1")
   }
 }
