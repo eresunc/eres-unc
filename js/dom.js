dd8f0b3079de3464a3aaf59c83639d6167a45ea7
eresunc <eres.unc.cajamarca@gmail.com>
2021-03-09 14:08:23 -0500

    Improve style position of imagen-logo

--- js/dom.js
+++ js/dom.js
@@ -3,15 +3,18 @@ window.onscroll = function () {
 }
 
 var navbar = document.getElementById("sticky-nav")
+var divImgLogo = document.getElementsByClassName("div-img-logo")[0]
 var redesCirculo = document.getElementById("redesCirculo")
 var sticky = navbar.offsetTop
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
+    divImgLogo.classList.add("flex-center")
     redesCirculo.classList.add("d-opacity-1")
   } else {
     navbar.classList.remove("sticky")
+    divImgLogo.classList.remove("flex-center")
     redesCirculo.classList.remove("d-opacity-1")
   }
 }
