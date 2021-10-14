31657166b43ee45e80186a5caf2ea8fd4b715b93
eresunc <eres.unc.cajamarca@gmail.com>
2021-10-13 19:25:08 -0500

    Add scroll functionality to avoid menu overlapping

--- v1.2/js/dom.js
+++ v1.2/js/dom.js
@@ -1,14 +1,32 @@
-window.onscroll = function () {
-  myFunction()
-}
+const navbar = document.getElementById("sticky-nav")
+const mainContainer = document.querySelector("body")
+const sticky = navbar.offsetTop
 
-var navbar = document.getElementById("sticky-nav")
-var sticky = navbar.offsetTop
-
-function myFunction() {
+function addStickyStyle() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
+    mainContainer.style.marginTop = '70px'
   } else {
     navbar.classList.remove("sticky")
+    mainContainer.style.marginTop = 'inherit'
   }
 }
+
+function addListenersToMenuOptions() {
+  const menuItems = document.querySelectorAll('.item-menu')
+
+  menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+      menuItems.forEach(it => it.classList.remove('active'))
+      item.classList.add('active')
+      setTimeout(() => {
+        window.scrollBy(0, -70)
+      }, 10)
+    })
+  })
+}
+
+window.onscroll = function () {
+  addStickyStyle()
+}
+addListenersToMenuOptions()
